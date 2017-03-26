b574cadd4a722ac2686a254431f22554935e0fc2
rtorres90 <roberto.torresdn@gmail.com>
2017-03-26 17:32:44 -0300

    added pip compatibility

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'cli-menu-helper',
+  packages = ['cli-menu-helper'],
+  version = '0.1',
+  description = 'Library to create cli menus',
+  author = 'Roberto Torres',
+  author_email = 'roberto.torres.dev@gmail.com',
+  url = 'https://github.com/rtorres90/cli-menu-helper/',
+  download_url = 'https://github.com/rtorres90/cli-menu-helper/archive/0.1.tar.gz',
+  keywords = ['menu', 'cli'],
+  classifiers = [],
+)
